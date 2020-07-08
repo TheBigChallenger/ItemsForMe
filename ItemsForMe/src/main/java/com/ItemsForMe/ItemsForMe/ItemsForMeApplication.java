df1eced18f7e864d934e33946fdc9a1634e8199c
User <User@Bernard>
2020-07-08 12:58:32 +0300

    Check in for the app

---
+++ ItemsForMe/src/main/java/com/ItemsForMe/ItemsForMe/ItemsForMeApplication.java
@@ -0,0 +1,13 @@
+package com.ItemsForMe.ItemsForMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsForMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsForMeApplication.class, args);
+	}
+
+}
---
+++ ItemsForMe/src/test/java/com/ItemsForMe/ItemsForMe/ItemsForMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ItemsForMe.ItemsForMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsForMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
